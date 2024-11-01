e20a37dad5ddbd6fd9dec251c652d89e8e0fc5d9
Mido <mohamedhassan3m3m3m@gmail.com>
2024-11-01 20:32:29 +0200

    first commit

---
+++ lab4.cpp
@@ -0,0 +1,10 @@
+using namespace std;
+#include <iostream>
+int sum(int s,int v) { 
+    return s + v;}
+int main(){
+    int a = 5, b = 10;
+    cout<<"sum "<<sum(a,b)<<endl;
+    
+    return 0;
+}
